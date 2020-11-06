26a4cec872a57e905bbd2376f7f68db86052a981
ck461sinu2020 <74027568+ck461sinu2020@users.noreply.github.com>
2020-11-06 15:57:42 +1100

    Add files via upload

---
+++ data/AOI_SolomonIslands_4.js
@@ -0,0 +1,8 @@
+var json_AOI_SolomonIslands_4 = {
+"type": "FeatureCollection",
+"name": "AOI_SolomonIslands_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 155.512275120262018, -12.30793592080828 ], [ 155.512275120262018, -5.092125963760306 ], [ 169.852552919991751, -5.092125963760306 ], [ 169.852552919991751, -12.30793592080828 ], [ 155.512275120262018, -12.30793592080828 ] ] ] ] } }
+]
+}
